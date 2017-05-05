34579a725f9abb0f3b85d6a4c8069e8ca9f780be
Marcelo <marcelo.dsantos@b2wdigital.com>
2017-05-05 18:34:00 -0300

    adicionado no pom o starter actuator e hystrix

--- src/main/java/com/eureka/client/EurekaClientApplication.java
+++ src/main/java/com/eureka/client/EurekaClientApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
 @EnableDiscoveryClient
+@EnableHystrix
 public class EurekaClientApplication {
 
 	public static void main(String[] args) {
